f76e8dbc0c157fec6aabbcbbe899c63beb8eaa53
Nicolas Bock <nicolasbock@gmail.com>
2021-10-17 20:33:59 -0600

    Update website build script
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- test-github-pages.sh
+++ test-github-pages.sh
@@ -4,9 +4,8 @@ set -e -u -x
 
 basedir=$(readlink --canonicalize $(dirname $0))
 
-docker run \
-  --volume ${basedir}/docs:/srv/jekyll \
-  --volume ${basedir}/docs/_site:/srv/jekyll/_site \
-  --publish 4000:4000 \
-  jekyll/builder:latest \
-  /bin/bash -c "jekyll serve --watch -H 0.0.0.0 --future"
+docker run --rm \
+  --volume="${basedir}/docs:/srv/jekyll" \
+  --publish [::1]:4000:4000 \
+  jekyll/jekyll \
+  jekyll serve
